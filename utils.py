4242ef020c64d88c9ee8a3a93368bcc8cd77adea
guspe98 <guspe98@gmail.com>
2020-03-25 16:42:59 -0400

    style fix

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.gridspec as gridspec
 from matplotlib.dates import datestr2num
+from datetime import timedelta
 
 
 def div0(n, d):
@@ -13,7 +14,7 @@ def build_plot(data, countries, data_type='Not specified', vis=40, dpi=100):
     elif data_type == 'Deaths':
         threshold = 500
     elif data_type == 'Mortality':
-        threshold = 0.025
+        threshold = 2.5
     countries = countries[::-1]
     myFmt = mdates.DateFormatter('%d-%m-%Y')
     fig, ax = plt.subplots()
@@ -24,23 +25,33 @@ def build_plot(data, countries, data_type='Not specified', vis=40, dpi=100):
                 if j < 100: # To have a representative (?) sample
                     res.append(0)
                 else:
-                    res.append(div0(i,j))
+                    res.append(div0(i,j)*100)
             ax.plot(mdates.num2date(datestr2num(data[country]['DATE'][-vis:])),res[-vis:],'.-')
+            plt.text(mdates.num2date(datestr2num(data[country]['DATE'][-1])) + timedelta(hours=5),
+                    res[-1] - 0.06, "{0:.2f}%".format(res[-1]), color='#555555')
     elif data_type == 'Cases':    
         for country in countries:
             ax.plot(mdates.num2date(datestr2num(data[country]['DATE'][-vis:])), 
                     data[country]['CUM_CASES'][-vis:],'.-')
+            plt.text(mdates.num2date(datestr2num(data[country]['DATE'][-1])) + timedelta(hours=5), 
+                    data[country]['CUM_CASES'][-1] - 400, 
+                    data[country]['CUM_CASES'][-1], color='#555555')
     elif data_type == 'Deaths':
         for country in countries:
             ax.plot(mdates.num2date(datestr2num(data[country]['DATE'][-vis:])), 
                     data[country]['CUM_DEATHS'][-vis:],'.-')
+            plt.text(mdates.num2date(datestr2num(data[country]['DATE'][-1])) + timedelta(hours=5),
+                    data[country]['CUM_DEATHS'][-1] - 40,
+                    data[country]['CUM_DEATHS'][-1], color='#555555')
     ax.legend(countries, loc='upper left', shadow=True)
     ax.set_ylabel(data_type)
     ax.set_xticks(mdates.num2date(datestr2num(data['Italy']['DATE'][0::1])))
     ax.grid(True, linestyle='--')
     ax.xaxis.set_major_formatter(myFmt)
     ax.set_facecolor('#ECECEC')
-    ax.fill_between(mdates.num2date(datestr2num(data['Italy']['DATE'][-vis:])), 0, 0.025, facecolor='#CCCCCC')
+    if data_type == 'Mortality':
+        ax.fill_between(mdates.num2date(datestr2num(data['Italy']['DATE'][-vis:])), 
+                0, threshold, facecolor='#CCCCCC')
     plt.xticks(rotation=45)
     plt.hlines(y=threshold, xmin=min(mdates.num2date(datestr2num(data['Italy']['DATE'][-vis:]))),
             xmax=max(mdates.num2date(datestr2num(data['Italy']['DATE']))), linestyles='dotted', color='w')
@@ -50,4 +61,5 @@ def build_plot(data, countries, data_type='Not specified', vis=40, dpi=100):
     plt.savefig(filename, dpi=dpi)
     plt.close()
     print("%s plot saved to file '%s'"%(data_type, filename))
+
 
